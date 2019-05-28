ca9d7e518ade148b4a8c1518d9f4d7273711de6d
springercass <casprin2@gmail.com>
2019-05-28 07:27:28 -0500

    wip header container js complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+import ImageThumbnail from "./ImageThumbnail";
+
+function HeaderContainer() {
+  return (
+    <div className="headerContainer">
+      <div className="thumbnail">
+        <ImageThumbnail />
+      </div>
+      <div className="headerText">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+}
+export default HeaderContainer;
