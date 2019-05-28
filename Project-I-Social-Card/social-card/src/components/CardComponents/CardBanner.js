45d2cffaf18aa6b8df496865e500e5e912dfd220
springercass <casprin2@gmail.com>
2019-05-28 07:39:48 -0500

    card content updated. wip css

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,4 +1,13 @@
 import React from "react";
 import "./Card.css";
 
-https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png
+const CardBanner = () => (
+  <div className="cardBanner">
+    <img
+      className="bannerImg"
+      src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+    />
+  </div>
+);
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
+const CardContent = () => (
+  <div className="cardContent">
+    <div className="titleContent">Get Started with React</div>
+    <div className="mainContent">
+      React makes it painless to create interactive UIs. Design simple views for
+      each state in your application.{" "}
+    </div>
+    <div className="linkContent">reactjs.org</div>
+  </div>
+);
 
-Get Started with React
-React makes it painless to create interactive UIs. Design simple views for each state in your application. 
-reactjs.org 
+export default CardContent;
