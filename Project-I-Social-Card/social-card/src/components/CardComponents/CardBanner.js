21872ff41755a16116ee96b5d6ee0b3d1adc2914
springercass <casprin2@gmail.com>
2019-05-28 07:31:12 -0500

    wip card container content

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,4 @@
 import React from "react";
 import "./Card.css";
+
+https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Card.css';
+
+
+Get Started with React
+React makes it painless to create interactive UIs. Design simple views for each state in your application. 
+reactjs.org 
